6e8bad363e94302d814f5c5e17600e7e491f0a84
BEAmaster <steamshi@gmail.com>
2018-11-13 15:04:12 +0800

    first web application.properties 内容为空，默认端口8080

--- lesson1/src/main/java/com/steam/lesson1/Lesson1Application.java
+++ lesson1/src/main/java/com/steam/lesson1/Lesson1Application.java
@@ -2,9 +2,19 @@ package com.steam.lesson1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
+@RestController
 @SpringBootApplication
 public class Lesson1Application {
+
+    @RequestMapping("/")
+    String index() {
+        return "Hello, Spring Boot"+ new Date();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Lesson1Application.class, args);
