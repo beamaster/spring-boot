a558422dc434af1597fc7df1ba2c9e43d0e7fc02
BEAmaster <steamshi@gmail.com>
2018-11-16 10:02:47 +0800

    jasypt 加密配置文件

--- lesson2/src/test/java/com/steam/lesson2/Lesson2ApplicationTests.java
+++ lesson2/src/test/java/com/steam/lesson2/Lesson2ApplicationTests.java
@@ -1,13 +1,26 @@
 package com.steam.lesson2;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Lesson2ApplicationTests {
+
+    @Autowired
+    private StringEncryptor stringEncryptor;
+
+    @Test
+    public void encryptProperties() {
+        System.out.println(stringEncryptor.encrypt("your actual username"));
+        System.out.println(stringEncryptor.encrypt("your actual password"));
+        System.out.println(stringEncryptor.encrypt("your actual jdbcUrl"));
+    }
+
 
     @Test
     public void contextLoads() {
